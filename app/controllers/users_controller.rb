7c2196b8279bf7161b0300bbc76cf9fd581c693b
Andy Kwan <zelthrox@gmail.com>
2016-03-03 20:16:39 +0000

    added admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+
   def new
     @user = User.new
   end
@@ -15,7 +20,7 @@ class UsersController < ApplicationController
 
   protected
   def user_params
-    params.require(:user).permit(:email, :firstname, :lastname, :password, :password_confirmation)
+    params.require(:user).permit(:email, :firstname, :lastname, :password, :password_confirmation, :admin)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,14 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create, :destroy]
-  root to: 'movies#index'
+  resource :session, only: [:new, :create, :destroy]
 
   namespace :admin do
-    resources :users
+    resources :users, only: [:index, :edit, :destroy]
   end
+
+  root to: 'movies#index'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
